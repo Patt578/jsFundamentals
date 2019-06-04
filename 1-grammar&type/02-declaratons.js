014dd01d55e669ec907a556a337bae0f4803b3dc
Patrick Bassett <bassettptty@gmail.com>
2019-06-04 09:50:21 -0400

    first commit

---
+++ 1-grammar&type/02-declaratons.js
@@ -0,0 +1,16 @@
+let a = 2;
+let b = 1;
+
+console.log(a+b);
+
+/**************************
+DECLARATIONS
+**************************/
+/*
+Declarations are the LEFT SIDE of a variable
+  It is simply the var x
+  It is on the left side of the assignment operator (=)
+Initializations are the RIGHT SIDE of a variable
+  It sets the value of the variable
+  It incorporates the variable name (x), the assignment operator (=), and the value (10) => x = 10
+*/
---
+++ 1-grammar&type/03-types.js
@@ -0,0 +1,39 @@
+/* 
+Booleans
+******************
+What is a boolean? Boolean can represent: true/false, yes/no, on/off
+*/ 
+let i = true;
+let j = false;
+/*        (1)
+1-Keyword for boolean - no quotation marks
+let on = true;
+console.log(on); // true
+let off = false;
+console.log(off); // false
+ */
+
+ /* 
+Null
+******************
+Null = empty value (not 0; not undefined)
+It is like an empty container; nothing is in it, but it still exists as a space to fill
+*/
+let empty = null;
+console.log(empty); // null
+/*
+Undefined
+******************
+Undefined = no value assigned (not even an empty container)
+UNDEFINED IS NOT AN ERROR
+*/
+let undef = undefined;// never do this
+console.log(undef); // undefined
+let grass;
+console.log(grass); // undefined
+/*
+Null vs. Undefined 
+******************
+Null is like a container with nothing in it
+Undefined is when a variable has never been set, or hasnt been created yet. 
+*/
